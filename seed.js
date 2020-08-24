e6b65cb3b1688dd095c14a07b210f13e59efc6c7
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-24 14:25:56 -0700

    tested data feeding into db and showing on index page

--- seed.js
+++ seed.js
@@ -4,7 +4,7 @@ const data = require('./ratingData.json')
 
 // create ratings from seed file
 db.Rating.deleteMany({}, (err, deletedRatings) => {
-    db.Rating.create(data.games, (err, seededRatings) => {
+    db.Rating.create(data.ratings, (err, seededRatings) => {
         if (err) console.log(err);
         
         console.log(data.ratings.length, "rating(s) created successfully")

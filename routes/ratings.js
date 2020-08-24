9d5691c878ec9bae84c1de54051653e236d043be
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-24 15:55:15 -0700

    debugged missing colon in show route

--- routes/ratings.js
+++ routes/ratings.js
@@ -5,7 +5,7 @@ const controller = require('../controllers')
 // routes for ratings
 // path = /api/v1/ratings
 router.get('/', controller.ratings.index)
-router.get('/id', controller.ratings.show)
+router.get('/:id', controller.ratings.show)
 router.post('/', controller.ratings.create)
 router.put('/:id', controller.ratings.update)
 router.delete('/:id', controller.ratings.destroy)

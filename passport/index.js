802a29d295f8ad8ece539e4c554c66c809446ae7
sbgarcia <sharonbondoc.g@gmail.com>
2020-08-30 05:51:17 -0700

    removed print statements from passport files

--- passport/index.js
+++ passport/index.js
@@ -7,19 +7,14 @@ const User = require('../models/user')
 
 // invoked at user log in to save the session through request.session.passport.user
 passport.serializeUser((user, done) => {
-    // console.log("passport/index.js: serializeUser function called")
-    // console.log(user)
     done(null, user._id)
 })
 
 // invoked anytime logged in user makes a request to the app
 // passport adds user info to request.user to verify as authenticated user
 passport.deserializeUser((id, done) => {
-    // console.log("passport/index.js: deserializeUser function called")
-
     User.findById(id, 'email', (err, user) => {
         if (err) return done(err, null)
-        // console.log(user)
         done(null, user)
     })
 }) 
